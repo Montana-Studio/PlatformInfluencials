768d511b515c13c9aaae341e7f583533e9867284
Mediatrends <mauro@mediatrends.cl>
2015-10-15 11:12:25 -0300

    update 15.10.2015

--- app/campana.php
+++ app/campana.php
@@ -1,150 +1,59 @@
 <?php include 'header.php'; ?>
 
-<?php 
-	if ($num_rows > 0){
-		echo '<h2 class="sub-titulo">campañas activas</h2><div class="creadas">';
+<?php
 
-	do{ 
-		echo '
-			
-			<script>
+		echo '<script>
 				$(document).ready(function(){
-					var foto;
-					$("#file'.$row[0].'").click(function (){
-						foto = "1";
+					$(".activar-campana").click(function (){
+							var idActualizar = this.id;
+							var idEstado = this.type;
+							alert(this.type);
+							var tipo = "activar";
+								$.ajax({
+									type: "POST",  
+									url: "procesar_eliminar-campana.php",  
+									data: "idActualizar="+idActualizar+"&idEstado="+idEstado+"&tipo="+tipo,
+									success: function(data){ 
+										window.location.reload();
+									}
+								});  
 					});
 
-					var info;
-					var idCampana = "'.$row[0].'";
-					var idAgencia = '.$_SESSION["id"].';
-					var correo ="'.$_SESSION["correo"].'";
-					var rsid ="'.$_SESSION["rsid"].'";
-					var tipo ="imagen";
+					$(".btneliminar").click(function (){
+						var idEliminar = this.id;
+						var tipo = "eliminar";
 
-					$("#campanaForm'.$row[0].'").on("submit",(function (e){
-						e.preventDefault();
-						info = new FormData(this);
-						info.append("nombre",$("#nombre-campana-"+idCampana+" input").val());
-						info.append("marca",$("#marca-campana-"+idCampana+" input").val());
-						info.append("descripcion",$("#descripcion-campana-"+idCampana+" textarea").val());
-						info.append("idCampana",idCampana);
-						info.append("idpersona",idAgencia);	
-						info.append("tipo",tipo);	
-						info.append("campana",idCampana);
-						info.append("id",idAgencia);
-						info.append("foto",foto);
-						$.ajax({
-								type: "POST",  
-								url: "procesar_imagen.php",  
-								data: info,
-								enctype: "multipart/form-data",
-								contentType: false,      
-								cache: false,             
-								processData:false,
-
-							success: function(data){ 
-								//console.log(data);
-							
-							}
-						
-						});
-					}));
-					
-					$("#activar-campana-'.$row[0].'").click(function (){
-						var idActualizar = '.$row[0].';
-						var idEstado = 0;
-						var tipo = "activar";
+						if (confirm("realmente desea eliminar la campaña  :"+idEliminar)) {
 							$.ajax({
 								type: "POST",  
 								url: "procesar_eliminar-campana.php",  
-								data: "idActualizar="+idActualizar+"&idEstado="+idEstado+"&tipo="+tipo,
+								data: "idEliminar="+idEliminar+"&tipo="+tipo,
+							
 								success: function(data){ 
 									window.location.reload();
 								}
 							});  
-						});
-				
-				});
-			</script>
-
-			
-				<div class="recientes">
+						}
+						return false; 
 					
-					<div class="cont-campana">
-						
-							<div class="bg-campana" style="background-image:url('.$row[3].');">
-								
-								<h3>'.$row[1].'<span>by '.$row[4].'</span></h3>
-								
+					});
 
-								<div class="edit-campana" style="display:none;float:left;clear:both;"></div>
+					var foto;
+					$(".file").click(function (){
+						foto = "1";
+					});
 
-							</div>
-
-							<div class="ver-mas"><span><i class="fa fa-angle-down"></i></span></div>
-							
-							<div class="content">
-								<div class="btn_close"><span><i class="fa fa-times-circle-o"></i></span></div>
-								<form id="campanaForm'.$row[0].'">		
-									
-									<ul class="tools-campana">
-										<li id="activar-campana-'.$row[0].'"><i class="tool-ico fa fa-remove"></i><span class="tool-txt"> desactivar</span></li>
-									</ul>
-									<div class="inputs-campana nombre" id="nombre-campana-'.$row[0].'">
-										<input placeholder="'.$row[1].'" disabled />
-									</div>
-
-									<div class="inputs-campana marca" id="marca-campana-'.$row[0].'">
-										<input  placeholder="by '.$row[4].'" disabled />
-									</div>
-									
-									<span class="campa-ico"><i class="fa fa-cog"></i>Activada</span>
-									<span class="campa-ico"><i class="fa fa-calendar"></i>02 Octubre 2015</span>
-									
-									<div class="inputs-campana descripcion" id="descripcion-campana-'.$row[0].'">
-										<textarea placeholder="descripcion" disabled>'.$row[2].'</textarea>
-									</div>
-
-									<!--button class="guardar-campana" type="submit" id="guardar-campana-'.$row[0].'">Guardar Cambios en '.$row[1].'</button-->
-									
-								</form>
-
-								<div class="img-compana-deskt hide">
-									<img src="'.$row[3].'"/>
-								</div>
-							</div>
-						
-					</div>
-
-				</div>
-		';
-	}while($row = mysqli_fetch_row($result));
-	echo '</div>';
-	}
-	if ($num_rows2 > 0){
-		echo '<h2 class="sub-titulo">campañas inactivas</h2><div class="creadas">';
-
-	do{ 
-		echo '
-		
-		
-		<script>
-			$(document).ready(function(){
-				var foto;
-
-				$("#file'.$row2[0].'").click(function (){
-					foto = "1";
-				});
-
+					
 				var info;
 				var idCampana = "'.$row2[0].'";
 				var idAgencia = '.$_SESSION["id"].';
 				var correo ="'.$_SESSION["correo"].'";
 				var rsid ="'.$_SESSION["rsid"].'";
 				var tipo ="imagen";
-				$("#campanaForm'.$row2[0].'").on("submit",(function (e){
+				$(".campanaForm").on("submit",(function (e){
 					e.preventDefault();
-					info = new FormData(this);
+					//alert(this.id);
+					/*info = new FormData(this);
 					info.append("nombre",$("#nombre-campana-"+idCampana+" input").val());
 					info.append("marca",$("#marca-campana-"+idCampana+" input").val());
 					info.append("descripcion",$("#descripcion-campana-"+idCampana+" textarea").val());
@@ -174,45 +83,215 @@
 								}
 						}
 					
+					});*/
+				
+				}));
+
+
+
+				});	
+			</script>';
+
+
+
+	if ($num_rows > 0){
+		echo '<h2 class="sub-titulo">campañas activas</h2><div class="creadas">';
+
+
+	do{ 
+		echo '
+			
+			<script>
+				$(document).ready(function(){
+					
+
+					var info;
+					var idCampana = "'.$row[0].'";
+					var idAgencia = '.$_SESSION["id"].';
+					var correo ="'.$_SESSION["correo"].'";
+					var rsid ="'.$_SESSION["rsid"].'";
+					var tipo ="imagen";
+					var foto;
+					$(".file").click(function (){
+						foto = "1";
 					});
+
+					$("#campanaForm'.$row[0].'").on("submit",(function (e){
+						e.preventDefault();
+						info = new FormData(this);
+		
+						info.append("idCampana",idCampana);
+						info.append("idpersona",idAgencia);	
+						info.append("tipo",tipo);	
+						info.append("campana",idCampana);
+						info.append("id",idAgencia);
+						info.append("foto",foto);
+						$.ajax({
+								type: "POST",  
+								url: "procesar_imagen.php",  
+								data: info,
+								enctype: "multipart/form-data",
+								contentType: false,      
+								cache: false,             
+								processData:false,
+
+							success: function(data){ 
+								//console.log(data);
+							
+							}
+						
+						});
+					}));
+					
+					
+				
+				});
+			</script>
+
+			
+				<div class="recientes">
+					
+					<div class="cont-campana">
+						
+							<div class="bg-campana" style="background-image:url('.$row[3].');">
+								
+								<h3>'.$row[1].'<span>by '.$row[4].'</span></h3>
+								
+
+								<div class="edit-campana" style="display:none;float:left;clear:both;"></div>
+
+							</div>
+
+							<div class="ver-mas"><span><i class="fa fa-angle-down"></i></span></div>
+							
+							<div class="content">
+								<div class="btn_close"><span><i class="fa fa-times-circle-o"></i></span></div>
+								<form class="campanaForm" id="'.$row[0].'">
+									<ul class="tools-campana">
+										<li class="activar-campana" type="1" id="'.$row[0].'" ><i class="tool-ico fa fa-remove"></i><span class="tool-txt"> desactivar</span></li>
+									</ul>
+									<div class="inputs-campana nombre nombre-campana" id="'.$row[0].'">
+										<input placeholder="'.$row[1].'" disabled />
+									</div>
+
+									<div class="inputs-campana marca marca-campana" id="'.$row[0].'">
+										<input  placeholder="by '.$row[4].'" disabled />
+									</div>
+									
+									<span class="campa-ico"><i class="fa fa-cog"></i>Activada</span>
+									<span class="campa-ico"><i class="fa fa-calendar"></i>02 Octubre 2015</span>
+									
+									<div class="inputs-campana descripcion descripcion-campana" id="'.$row[0].'">
+										<textarea placeholder="descripcion" disabled>'.$row[2].'</textarea>
+									</div>
+
+									<!--button class="guardar-campana" type="submit" id="guardar-campana-'.$row[0].'">Guardar Cambios en '.$row[1].'</button-->
+									
+								</form>
+
+								<div class="img-compana-deskt hide">
+									<img src="'.$row[3].'"/>
+								</div>
+							</div>
+						
+					</div>
+
+				</div>
+		';
+	}while($row = mysqli_fetch_row($result));
+	echo '</div>';
+	}
+	if ($num_rows2 > 0){
+		echo '<h2 class="sub-titulo">campañas inactivas</h2><div class="creadas">';
+
+	do{ 
+		echo '
+		
+		
+		<script>
+
+
+			/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************/
+
+
+					
+			$(document).ready(function(){
+				var foto;
+					$(".file").click(function (){
+						foto = "1";
+					});
+	
+				var info;
+				var idAgencia = '.$_SESSION["id"].';
+				var correo ="'.$_SESSION["correo"].'";
+				var rsid ="'.$_SESSION["rsid"].'";
+				var tipo ="imagen";
+				$(".campanaForm").on("submit",(function (e){
+					e.preventDefault();
+					info = new FormData(this);
+					//info.append("nombre",$("#this.id).val());
+					var id = this.id;
+					alert(id);
+					var nombre = $("#"+id+" .nombre input").val();
+					alert(nombre);
+
+
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************
+					/*********************************************************************************/
+					/*info.append("marca",$("#marca-campana-"+idCampana+" input").val());
+					info.append("descripcion",$("#descripcion-campana-"+idCampana+" textarea").val());
+					info.append("idCampana",this.id);
+					info.append("idpersona",idAgencia);	
+					info.append("tipo",tipo);	
+					info.append("campana",this.id);
+					info.append("id",idAgencia);
+					info.append("foto",foto);
+					//console.log(foto);
+					$.ajax({
+							type: "POST",  
+							url: "procesar_imagen.php",  
+							data: info,
+							enctype: "multipart/form-data",
+							contentType: false,      
+							cache: false,             
+							processData:false,
+
+						success: function(data){ 
+								switch (data){
+									case "nuevo": 	alert("registro actualizado");
+													window.location.reload();
+									break;
+									default: alert("problema con el tamaño o formato de la imagen");
+									break;
+								}
+						}
+					
+					});*/
 				
 				}));					
 		
-				$("#btneliminar'.$row2[0].'").click(function (){
-					var idEliminar = '.$row2[0].';
-					var tipo = "eliminar";
+			
 
-					if (confirm("realmente desea eliminar la campaña  :  '.$row2[1].'")) {
-						$.ajax({
-							type: "POST",  
-							url: "procesar_eliminar-campana.php",  
-							data: "idEliminar="+idEliminar+"&tipo="+tipo,
-						
-							success: function(data){ 
-								window.location.reload();
-							}
-						});  
-					}
-					return false; 
-				
-				});
 
-				$("#activar-campana-'.$row2[0].'").click(function (){
-					var idActualizar = '.$row2[0].';
-					var idEstado = 1;
-					var tipo = "activar";
-						$.ajax({
-							type: "POST",  
-							url: "procesar_eliminar-campana.php",  
-							data: "idActualizar="+idActualizar+"&idEstado="+idEstado+"&tipo="+tipo,
-							success: function(data){ 
-								window.location.reload();
-							}
-						});  
-					});
-
-				});
-		</script>
+			});
+			</script>
 			<div class="recientes">
 				<div class="cont-campana">
 
@@ -226,16 +305,16 @@
 
 					<div class="content">
 						<div class="btn_close"><span><i class="fa fa-times-circle-o"></i></span></div>
-						<form id="campanaForm'.$row2[0].'">
+						<form class="campanaForm" id="'.$row2[0].'">
 
 							<ul class="tools-campana">
 								<li class="edit-campanas"><i class="tool-ico fa fa-pencil"></i><span class="tool-txt"> editar</span></li>
-								<li id="activar-campana-'.$row2[0].'"><i class="tool-ico fa fa-check"></i><span class="tool-txt"> activar</span></li>
-								<li class="btneliminar" id="btneliminar'.$row2[0].'"><i class="tool-ico fa fa-trash-o"></i><span class="tool-txt"> eliminar</span></li>
+								<li class="activar-campana" id="'.$row2[0].'" name="0"><i class="tool-ico fa fa-check"></i><span class="tool-txt"> activar</span></li>
+								<li class="btneliminar" id="'.$row2[0].'"><i class="tool-ico fa fa-trash-o"></i><span class="tool-txt"> eliminar</span></li>
 							</ul>
 							
 							<div class="inputs-campana nombre" id="nombre-campana-'.$row2[0].'">
-								<input placeholder="'.$row2[1].'" disabled></input>
+								<input placeholder="'.$row2[1].'" class="nombre-input" disabled></input>
 								<i class="fa fa-pencil"></i>
 							</div>
 
@@ -256,7 +335,7 @@
 									$("div.jfilestyle").hide();
 								});
 							</script>
-							<input type="file" name="file" class="jfilestyle upload-img-campana" data-input="false" id="file'.$row2[0].'" data-buttonText="subir archivo"/>
+							<input type="file" name="file" class="jfilestyle upload-img-campana file" data-input="false" id="file'.$row2[0].'" data-buttonText="subir archivo"/>
 							
 							<button class="guardar-campana" type="submit" id="guardar-campana-'.$row2[0].'">Guardar Campaña</button>
 						
