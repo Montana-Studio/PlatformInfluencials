1911b1c79731b74868f0af95e9d8256382b64776
argkont <kohacaelo@gmail.com>
2015-11-10 12:07:45 -0300

    update 10/11 API instagram

--- app/RRSS/Instagram/procesar_instagram.php
+++ app/RRSS/Instagram/procesar_instagram.php
@@ -9,8 +9,9 @@ $json_user= file_get_contents($json_user_url);
 $links_user_url= json_decode($json_user);
 $followers_instagram = $links_user_url->data->counts->followed_by;
 $query="SELECT rrss_id FROM rrss WHERE rrss_id=".$instagramId." AND persona_id=".$_SESSION['id']."  AND descripcion_rrss='instagram'";
-$results1 = $mysqli->query($query);
 echo $query;
+$results1 = $mysqli->query($query);
+
 $num_row1=mysqli_num_rows($results1);
 
 if($num_row1 < 1){
