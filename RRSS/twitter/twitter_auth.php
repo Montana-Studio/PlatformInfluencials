105dd7d19914b24c1bf07aef8a5125ec212f5fbb
argkont <kohacaelo@gmail.com>
2015-11-17 12:14:07 -0300

    update BRANCH new

--- RRSS/twitter/twitter_auth.php
+++ RRSS/twitter/twitter_auth.php
@@ -3,7 +3,4 @@
 define('CONSUMER_KEY', 'hV95sLlCLjKIQbsVx1uVIxgKQ');
 define('CONSUMER_SECRET', 'FU3GBmbIldTUzJZJOJqrynhiiecmt2FPHAShlkGi3AH8jY7GrV');
 define('OAUTH_CALLBACK', 'http://desarrollo.adnativo.com/pi/app/rrss/twitter/process.php');
-<<<<<<< HEAD
-=======
 ?>
->>>>>>> 421ea01836da34dd44543af8c500a649d6694a14
