236ffc75fe8784fd17e7f7f4e1053c062468d873
argkont <kohacaelo@gmail.com>
2015-12-17 17:13:06 -0300

    update 17/11 Front varios

--- app/footer.php
+++ app/footer.php
@@ -70,7 +70,7 @@
 							}
 
 					});
-					$(".btn_close").on("click",function(){
+					$(".content .btn_close").on("click",function(){
 							$(this).closest(".content").fadeOut();
 
 							if(document.documentElement.clientWidth >= 1024){
@@ -129,8 +129,8 @@
 	<script type="text/javascript" src="js/jquery-filestyle.min.js"></script>
 
 	<script type="text/javascript" src="js/svg.min.js"></script>
-	<script type="text/javascript" src="js/svg.easing.min.js"></script>
-		<script>
+	<script async type="text/javascript" src="js/svg.easing.min.js"></script>
+	<script async>
 		//Animate SVG's
 		var icoHandLike = SVG('ico-handLike').size('100%','100%').viewbox(0, 0, 150, 150).addClass('likeHand')
 		var icoTrash = SVG('ico-trash').size('100%','100%').viewbox(50, 10.468, 29.875, 30.032).addClass('trash');
