b2218102eb8e8d4553f0749098ac8bf477e30866
Mediatrends <mauro@mediatrends.cl>
2015-12-18 12:08:25 -0300

    update 18.12.2015

---
+++ app/procesar_registro_ipe_facebook.php
@@ -0,0 +1,24 @@
+<?php 
+ require('conexion.php');
+$perfil= $_POST['perfil'];
+$region= $_POST['region']; 
+$comuna= $_POST['comuna'];
+$id=$_SESSION['id'];
+
+if(strlen($perfil)>0&&strlen($perfil)>0&&strlen($perfil)>0){
+	
+	if($perfil==3){
+	$descripcion_tipo='influenciador';
+	}else if($perfil==4){
+	$descripcion_tipo='publicador';
+	}else if($perfil==5){
+	$descripcion_tipo='editor';
+	}
+	$results2 = $mysqli->query("UPDATE persona SET id_tipo='$perfil', descripcion_tipo='$descripcion_tipo', region='$region', comuna='$comuna', estado_formulario='1' WHERE id='$id'");
+	$resultado = 'actualizado';
+}else{
+	$resultado = 'false';
+}
+
+echo $resultado;
+ ?>
