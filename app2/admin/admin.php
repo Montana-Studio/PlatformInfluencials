e50e10b1cd78177bae6958a6f05cd6a485689c1a
argkont <kohacaelo@gmail.com>
2016-04-07 11:29:27 -0300

    update 07/04 delete app2

--- app2/admin/admin.php
+++
@@ -1,5 +0,0 @@
-<?php include 'app_admin/http/controllers/header.php';
-	  require('app_admin/http/controllers/conexion.php');
- ?>
- </body>
-</html>
--- app2/newsletter/news.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('../controller/conexion.php');
-$nombre=$_POST['nombre'];
-$correo=$_POST['correo'];
-$perfil=$_POST['perfil'];
-$results = $mysqli->query('INSERT INTO newsletter (nombre, correo, tipo)VALUES ("'.$nombre.'", "'.$correo.'","'.$perfil.'")');
-?>
